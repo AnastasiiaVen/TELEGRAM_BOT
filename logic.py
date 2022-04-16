e8a05dd09ea0f3863889a69e0b3a3f9f94ec1d56
Vyacheslav <dobrov.slava@yandex.ru>
2022-04-16 19:45:34 +0300

    Changed named of value 'avilable' in 'available'

--- logic.py
+++ logic.py
@@ -2,15 +2,18 @@ import random
 import back_logic as bl
 
 
-board = list(range(1,10))
+board = list(range(1, 10))
+
 
 def draw_board(board):
-    print ("-" * 13)
+    print("-" * 13)
     for i in range(3):
-        print ("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
-        print ("-" * 13)
+        print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
+        print("-" * 13)
+
+
 def take_NPC_turn(player_token):
-    win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
     valid = False
     while not valid:
         for each in win_coord:
@@ -18,33 +21,32 @@ def take_NPC_turn(player_token):
                 NPC_turn = random.randint(0, len(each) - 1)
                 valid = True
         else:
-            avalible = False
-            while not avalible:   
+            available = False
+            while not available:
                 NPC_turn = random.randint(1, 9)
-                if (str(board[NPC_turn-1]) not in "XO"):
-                    board[NPC_turn-1] = player_token
-                    avalible = True
+                if (str(board[NPC_turn - 1]) not in "XO"):
+                    board[NPC_turn - 1] = player_token
+                    available = True
             valid = True
-
 
 
 def take_input(player_token):
     valid = False
     while not valid:
-        player_answer = input("Куда поставим " + player_token+"? ")
+        player_answer = input("Куда поставим " + player_token + "? ")
         try:
             player_answer = int(player_answer)
         except:
-            print ("Некорректный ввод. Вы уверены, что ввели число?")
+            print("Некорректный ввод. Вы уверены, что ввели число?")
             continue
         if player_answer >= 1 and player_answer <= 9:
-            if (str(board[player_answer-1]) not in "XO"):
-                board[player_answer-1] = player_token
+            if (str(board[player_answer - 1]) not in "XO"):
+                board[player_answer - 1] = player_token
                 valid = True
             else:
-                print ("Эта клеточка уже занята")
+                print("Эта клеточка уже занята")
         else:
-            print ("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
+            print("Некорректный ввод. Введите число от 1 до 9, чтобы сделать ход.")
 
 
 def main(board):
@@ -57,19 +59,18 @@ def main(board):
         else:
             take_NPC_turn("O")
             print('Ход NPC.')
-            #take_input("O")
+            # take_input("O")
         counter += 1
         if counter > 4:
             tmp = bl.check_win(board)
             if tmp:
-                print (tmp, "выиграл!")
+                print(tmp, "выиграл!")
                 win = True
                 break
         if counter == 9:
-            print ("Ничья!")
+            print("Ничья!")
             break
     draw_board(board)
 
+
 main(board)
-
-
